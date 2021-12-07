5cc9822661f952367512b5481203a9f2c6158cb9
Miroslav <miroslavmatovic@github.com>
2021-12-07 16:35:07 +0300

    initial

---
+++ testingTeleport/testingTeleport/testingTeleportApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingTeleportApp.swift
+//  testingTeleport
+//
+//  Created by Admin on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct testingTeleportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
